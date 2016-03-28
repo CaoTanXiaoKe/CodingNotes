57cda7ca56da5ff816e53e34c85833f811eda084
CaoTanXiaoKe <2568333286@qq.com>
2016-03-28 23:22:57 +0800

    布局管理器之FlowLayout

--- java/Login2.java
+++ java/Login2.java
@@ -10,6 +10,6 @@ public class Login2 {
 		f.add(b);
 		f.setBackground(Color.lightGray);
 		f.setVisible(true);
-		f.pack();
+		f.pack();// 因为login按钮有默认大小，Frame大小不确定，调用Frame对象的pack方法， 使得Frame按照组件当前的合适的尺寸（preferred size）进行相应的调整。 
 	}
 }
